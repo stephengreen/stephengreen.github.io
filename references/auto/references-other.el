07943b275a2eecf535934178c70ad6964428b67a
Stephen Green <stephenrolandgreen@gmail.com>
2026-05-24 13:15:16 -0400

    update site and add dingo-pop post

---
+++ references/auto/references-other.el
@@ -0,0 +1,24 @@
+(TeX-add-style-hook
+ "references-other"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Adams:2013vsa"
+    "Bizon:2011gg"
+    "Buchert:2015iva"
+    "Chan:2025pdf"
+    "Chan:2025kyu"
+    "Essick:2022ojx"
+    "Farr:2019rap"
+    "Fishbach:2018edt"
+    "Heinzel:2025ogf"
+    "LIGOScientific:2019hgc"
+    "Mandel:2018mve"
+    "Moschidis:2017llu"
+    "Moschidis:2018ruk"
+    "Ramos-Buades:2023yhy"
+    "Santoliquido:2025aiq"
+    "Talbot:2023pex"
+    "Vitale:2020aaz"
+    "Yang:2014tla"))
+ :bibtex)
+
---
+++ scripts/merge_bibs.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+# Concatenates the project's .bib files into references/_all.bib so that
+# Obsidian's Citations plugin (which only accepts a single path) sees both
+# the INSPIRE-fetched and the manually-curated entries.
+#
+# Wired into Quarto via the pre-render hook in _quarto.yml. Can also be run
+# manually if you're editing references without `quarto preview` running.
+
+set -euo pipefail
+DIR="$(cd "$(dirname "$0")/.." && pwd)/references"
+cat "$DIR/INSPIRE.bib" "$DIR/references-other.bib" > "$DIR/_all.bib"
